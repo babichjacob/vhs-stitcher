a8828cdde7dbb7d74ba08d63ff749f8d8a65debc
Jacob Babich <jacobbabichpublic+git@gmail.com>
2020-03-28 14:55:29 -0400

    use config file and make cli

--- __main__.py
+++ __main__.py
@@ -1,58 +1,81 @@
+from imageio import imwrite
+from itertools import islice
 from ml_datasets import mnist
-from thinc.api import Adam, chain, fix_random_seed, prefer_gpu, Relu, Softmax
-from tqdm.notebook import tqdm
+from numpy import array
+from pathlib import Path
+from thinc.api import Adam, chain, Config, fix_random_seed, Model, prefer_gpu, registry
+from tqdm import tqdm
 
-if __name__ == "__main__":
-    is_gpu = prefer_gpu()
-    print("using gpu" if is_gpu else "not using gpu")
 
-    (train_X, train_Y), (dev_X, dev_Y) = mnist()
+def train_model(data, model, optimizer, n_iter, batch_size):
+    (train_X, train_Y), (dev_X, dev_Y) = data
+    indices = model.ops.xp.arange(train_X.shape[0], dtype="i")
 
-    print(f"Training size: {len(train_X)}")
-    print(f"Dev size: {len(dev_X)}")
-
-    # Neurons per hidden layer
-    n_hidden = 32
-    # Dropout ratio
-    dropout = 0.2
-
-    model = chain(
-        Relu(nO=n_hidden, dropout=dropout),
-        Relu(nO=n_hidden, dropout=dropout),
-        Softmax(),
-    )
-
-    [train_X, train_Y, dev_X, dev_Y] = [model.ops.asarray(
-        item) for item in [train_X, train_Y, dev_X, dev_Y]]
-    
-    model.initialize(X=train_X[:5], Y=train_Y[:5])
-    nI = model.get_dim("nI")
-    nO = model.get_dim("nO")
-    print(f"Initialized model with input dimension nI={nI} and output dimension nO={nO}")
-
-    fix_random_seed(0)
-    optimizer = Adam(0.001)
-    batch_size = 128
-    print(f"Measuring performance across iterations:")
-
-    for i in range(10):
-        batches = model.ops.multibatch(batch_size, train_X, train_Y, shuffle=True)
+    for i in range(n_iter):
+        batches = model.ops.multibatch(
+            batch_size, train_X, train_Y, shuffle=True)
 
         for X, Y in tqdm(batches, leave=False):
             Yh, backpropagate = model.begin_update(X)
             backpropagate(Yh - Y)
             model.finish_update(optimizer)
-        
-        # Evaluate and print progress
+
         correct = 0
         total = 0
-        
+
         for X, Y in model.ops.multibatch(batch_size, dev_X, dev_Y):
             Yh = model.predict(X)
             correct += (Yh.argmax(axis=1) == Y.argmax(axis=1)).sum()
             total += Yh.shape[0]
-        
+
         score = correct / total
 
-        print(f" {i} {score:.3f}")
+        print(f"  {i}: {float(score):.3f}")
 
+
+def main(*, train: bool = False, examine_data: bool = False):
+    is_gpu = prefer_gpu()
+    print("using gpu" if is_gpu else "not using gpu")
+
+    this_directory = Path(__file__).parent
+
+    with open(this_directory / "thinc.ini") as config_ini:
+        config_str = config_ini.read()
+
+    config = Config().from_str(config_str)
+    loaded_config = registry.make_from_config(config)
+
+    model = loaded_config["model"]
+    optimizer = loaded_config["optimizer"]
+    n_iter = loaded_config["training"]["n_iter"]
+    batch_size = loaded_config["training"]["batch_size"]
+
+    (train_X, train_Y), (dev_X, dev_Y) = mnist()
+
+    fix_random_seed(0)
+    model.initialize(X=train_X[:5], Y=train_Y[:5])
+
+    if train:
+        print("Training:")
+        train_model(((train_X, train_Y), (dev_X, dev_Y)),
+                    model, optimizer, n_iter, batch_size)
+    elif examine_data:
+        print("Take a look at this data:")
+
+        image_directory = this_directory / "images"
+        image_directory.mkdir(exist_ok=True)
+
+        for i, (X, _) in tqdm(enumerate(zip(train_X, train_Y))):
+            if i > 20:
+                break
+
+            size = int(len(X)**0.5)
+            unflattened = array(
+                [array([X[i * size + j]*255 for j in range(size)]) for i in range(size)])
+
+            imwrite(image_directory / f"{i}.png", unflattened)
+
+
+if __name__ == "__main__":
+    from fire import Fire
+    Fire(main)
