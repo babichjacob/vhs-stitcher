58a5df976cc2fa7c30b0138cdd638739446bfa7a
Jacob Babich <jacobbabichpublic+git@gmail.com>
2020-04-12 21:55:16 -0400

    add Windows `stitch` command file (since I work on there now too since segfaults on AMD macOS)

---
+++ stitch.cmd
@@ -0,0 +1,17 @@
+rem https://stackoverflow.com/a/761658
+
+set arg1=%1
+
+@echo off
+rem throw the first parameter away
+shift
+set params=%1
+:loop
+shift
+if [%1]==[] goto afterloop
+set params=%params% %1
+goto loop
+:afterloop
+@echo on
+
+py -m vhs_stitcher.%arg1% %params%
